d9495c904f22933ffeed40919a11dd60cb64fa9c
Dimitrije090 <dimitrije.protic@gmail.com>
2020-12-30 18:25:04 +0100

    Skoro Final Version
    Dodao da radi kako treba i van browsera
    Popravio konverziju za final i allday
    
    Popravio logistiku da radi sa ukupnim sekundama a ne da zabelezava dane sate i minute pa da oduzima nego ovako samo prati stotinke pretvori ih u sekunde i onda to pretvori u H,M,S

--- javascript.js
+++ javascript.js
@@ -6,17 +6,7 @@ var timerRunning = false, yellow = false;
 var finalS = 0,finalM = 0,finalH = 0;
 var todayS = 0, todayM = 0, todayH = 0;
 
-
-
-  var d = new Date();
-
-  var currentDay = d.getDay();
-  var currentHour = d.getHours();
-  var currentMinute = d.getMinutes();
-  var currentSec = d.getSeconds();
-
-  
-  var exitD = 0,exitH = 0,exitM = 0,exitS = 0, exitIsRunning;
+var date,timeMili;
  
 
 //VARIABLE SAVE ---------------------------------------------------------------------------------------------------
@@ -26,7 +16,6 @@ var todayS = 0, todayM = 0, todayH = 0;
     	document.getElementById("lastRun").textContent = "";
 
 chrome.storage.local.get(['f_H'], function(result) {
-  console.log('Sati currently is ' + result.f_H);
      finalH = result.f_H;
 
      if(finalH < 10){
@@ -39,7 +28,6 @@ chrome.storage.local.get(['f_H'], function(result) {
 
 
 chrome.storage.local.get(['f_M'], function(result) {
-  console.log('Minuti currently is ' + result.f_M);
      finalM = result.f_M;
 
      if(finalM < 10){
@@ -51,7 +39,6 @@ chrome.storage.local.get(['f_M'], function(result) {
 
 
 chrome.storage.local.get(['f_S'], function(result) {
-  console.log('Sekunde currently is ' + result.f_S);
      finalS = result.f_S;
 
      if(finalS < 10){
@@ -71,8 +58,6 @@ chrome.storage.local.get(['f_S'], function(result) {
 
 
 chrome.storage.local.get(['t_H'], function(result) {
-  console.log('Sati currently is ' + result.t_H);
-
   if(result.t_H === undefined){}else{
   	     todayH = result.t_H;
   }
@@ -87,8 +72,6 @@ chrome.storage.local.get(['t_H'], function(result) {
 
 
 chrome.storage.local.get(['t_M'], function(result) {
-  console.log('Minuti currently is ' + result.t_M);
-
   if(result.t_M === undefined){}else{
   	     todayM = result.t_M;
   }
@@ -102,16 +85,9 @@ chrome.storage.local.get(['t_M'], function(result) {
 
 
 chrome.storage.local.get(['t_S'], function(result) {
-  console.log('Sekunde currently is ' + result.t_S);
-
-
   if(result.t_S === undefined){}else{
   	     todayS = result.t_S;
   }
-
-
-	console.log("DAANASS MAXX:" + todayS);
-console.log("SACUVANIII MAXX:" + result.t_S);
 
 
      if(todayS < 10){
@@ -125,33 +101,102 @@ console.log("SACUVANIII MAXX:" + result.t_S);
 
 //VARIABLE SAVE ---------------------------------------------------------------------------------------------------
 
-
-
-chrome.storage.local.get(['startedTheTimer'], function(result) {
-     exitIsRunning = result.startedTheTimer;
-
-    if(exitIsRunning == true){
-
-//treba da se povuku sve exit vrednosti
-           
-chrome.storage.local.get(['startD'], function(result) {
-     exitD = result.startD;
+//PROVERAVA DA LI JE DRUGI DAN-----------------------------------------------
 
 
 
 
+       var d_new = new Date();
+       var n_new = d_new.getDate();
 
-});  
+       console.log(localStorage.getItem('dateStarted'));
+       
+       var n_old = JSON.parse(localStorage.getItem('dateStarted'));
 
+
+    if(n_new != n_old){
+
+       chrome.storage.local.set({'t_S': 0}, function() {
+     });
+
+	   chrome.storage.local.set({'t_M': 0}, function() {
+     });
+
+		chrome.storage.local.set({'t_H': 0}, function() {
+     });
 
 
 
 
     }
 
-});
 
 
+
+
+//PROVERA DA LI JE UPALJEN ILI NIJE-------------------------------------------
+var tempIsRunnig = JSON.parse(localStorage.getItem('isTimerRunning'));
+var tempMili = JSON.parse(localStorage.getItem('startMili'));
+var finalMili;
+
+var currDate = new Date();
+var currMili = currDate.getTime();
+
+if(tempIsRunnig == true){
+
+
+	document.getElementById("mainTxt").textContent="Loading...";
+
+	document.body.style.setProperty('--dot-color',"#C05341");
+	document.body.style.setProperty('--circle-color',"#C05341");
+
+ finalMili = ~~((currMili - tempMili)/1000);
+
+ console.log("Final Sekunde " + finalMili);
+
+
+secondsToHms(finalMili);
+
+
+timerRunning = true;
+
+
+}else if(tempIsRunnig == false){
+
+    document.body.style.setProperty('--dot-color',"#81B898");
+	document.body.style.setProperty('--circle-color',"#81B898");
+
+
+timerRunning = false;
+
+
+    }else{
+
+    document.body.style.setProperty('--dot-color',"#81B898");
+	document.body.style.setProperty('--circle-color',"#81B898");
+
+timerRunning = false;
+ }
+
+
+
+function secondsToHms(d) {
+    d = Number(d);
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+
+    console.log("H su " + h);
+    console.log("M su " + m);
+    console.log("S su " + s);
+
+    hours = h;
+    minutes = m;
+    seconds = s;
+
+
+    timer();
+}
 
 
 
@@ -161,6 +206,7 @@ chrome.storage.local.get(['startD'], function(result) {
 
 
 //PROVERAVA DA LI DA PRIKAZE SVE NORMALNO ILI DA PRIKAZE VREME UMESTO STARTA ----------------------------------------------------
+
 
 
 
@@ -176,6 +222,8 @@ function add() {
     }
     
     h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+
 
     timer();
 }
@@ -215,13 +263,21 @@ function StartTimer(){
 
 		timerRunning = true;
 
-		chrome.storage.local.set({'startedTheTimer': timerRunning}, function() {});
+
+     date = new Date();
+     timeMili = date.getTime();
 
 
-		chrome.storage.local.set({'startD': currentDay}, function() {});
-	    chrome.storage.local.set({'startH': currentHour}, function() {});
-		chrome.storage.local.set({'startM': currentMinute}, function() {});
-		chrome.storage.local.set({'startS': currentSec}, function() {});
+    localStorage.setItem("startMili", timeMili);
+
+    localStorage.setItem("isTimerRunning", timerRunning);
+
+
+
+       var d = new Date();
+       var n = d.getDate();
+
+        localStorage.setItem("dateStarted", n);
 
 }
 
@@ -236,16 +292,17 @@ function StopTimer(){
 
 	    		timerRunning = false;
 
-		chrome.storage.local.set({'startedTheTimer': timerRunning}, function() {});
+        localStorage.setItem("isTimerRunning", timerRunning);
 
 
 	    		yellow = true;
 }
 
+
+
 function ClearTimer(){
 
-    console.log("ClearFunkcija");
-
+	var temp;
 
 	 h1.textContent = "00:00:00";
 
@@ -256,6 +313,20 @@ function ClearTimer(){
     todayS = todayS + finalS;
     todayM = todayM + finalM;
     todayH = todayH + finalH;
+
+    if (todayS >= 60) {
+    	temp =  Math.floor(todayS / 60)
+    	    todayM += Math.floor(todayS / 60);
+    	    todayS += todayS - temp * 60;
+        }
+
+
+    if (todayM >= 60) {
+    	temp =  Math.floor(todayM / 60)
+    	    todayH += Math.floor(todayM / 60);
+    	    todayM += todayM - temp * 60;
+        }
+
 
 
     	document.getElementById("lastRun").textContent = "";
@@ -279,18 +350,15 @@ function ClearTimer(){
      }
 
 
-
 	chrome.storage.local.set({'f_S': finalS}, function() {
-    console.log('Sacuvane sekunde');
      });
 
 		chrome.storage.local.set({'f_M': finalM}, function() {
-    console.log('Sacuvane minuti');
      });
 
 			chrome.storage.local.set({'f_H': finalH}, function() {
-    console.log('Sacuvane sati');
      });
+
 
 
 
@@ -316,15 +384,12 @@ function ClearTimer(){
 
 
 	chrome.storage.local.set({'t_S': todayS}, function() {
-    console.log('Sacuvane sekunde -- danas');
      });
 
 		chrome.storage.local.set({'t_M': todayM}, function() {
-    console.log('Sacuvane minuti -- danas');
      });
 
 			chrome.storage.local.set({'t_H': todayH}, function() {
-    console.log('Sacuvane sati -- danas');
      });
 
 
@@ -337,7 +402,7 @@ function ClearTimer(){
 
 		  timerRunning = false;
 
-		chrome.storage.local.set({'startedTheTimer': timerRunning}, function() {});
+
 
           yellow = false;
 }
